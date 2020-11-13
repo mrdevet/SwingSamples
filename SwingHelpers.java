5ee385f4e94e309725f16b4f94d7638341224863
Casey Devet <casey.devet@sccdsb.net>
2020-11-13 08:12:34 -0500

    Added isCollision method

--- SwingHelpers.java
+++ SwingHelpers.java
@@ -11,4 +11,13 @@ public class SwingHelpers {
 		Image scaledImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
 		return new JLabel(new ImageIcon(scaledImage));
 	}
+
+
+	/** Checks to see if two components collide. */
+	public static boolean isCollision (JComponent comp1, JComponent comp2) {
+		return comp1.getX() + comp1.getWidth() > comp2.getX() &&
+				comp1.getX() < comp2.getX() + comp2.getWidth() &&
+				comp1.getY() + comp1.getHeight() > comp2.getY() &&
+				comp1.getY() < comp2.getY() + comp2.getHeight();
+	}
 }
